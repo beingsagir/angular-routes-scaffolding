3e18e8ca26e07b17f7e59435e3e661d2a44caece
Sagiruddin Mondal <Sagiruddin Mondal>
2017-05-17 09:45:08 +0530

    angular4 routing practices

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,40 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { Router } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { CmpOneComponent } from './cmp-one/cmp-one.component';
+import { CmpTwoComponent } from './cmp-two/cmp-two.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+
+const appRoutes: Routes = [
+  { path: 'compOne', component: CmpOneComponent },
+  {
+    path: 'compTwo',
+    component: CmpTwoComponent,
+     pathMatch: 'full'
+  },
+  { path: '',
+    redirectTo: '/compOne',
+    pathMatch: 'full'
+  },
+  { path: '**', component: PageNotFoundComponent }
+];
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CmpOneComponent,
+    CmpTwoComponent,
+    PageNotFoundComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule,
     FormsModule,
     HttpModule
@@ -17,4 +43,10 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+   constructor(router: Router) {
+    console.log('Routes: ', JSON.stringify(router.config, undefined, 2));
+  }
+
+ }
